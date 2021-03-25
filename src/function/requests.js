8c0246e6720cae8b930a493f0bb06e1d78988e34
faustabmnyh <faustaabimanyu@gmail.com>
2021-03-25 15:05:11 +0700

    add env file

--- src/function/requests.js
+++ src/function/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "90ba3f0b7bf0a895f0a99539e4fb59ed";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
